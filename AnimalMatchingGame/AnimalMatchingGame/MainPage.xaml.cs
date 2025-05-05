df0e5d92d20a8ded1f1739d70d179697ced9a759
Petit-Nic <petitnicola@gmail.com>
2025-05-05 11:58:41 -0500

    Updated structure of repo. Finished Ch 2.

---
+++ AnimalMatchingGame/AnimalMatchingGame/MainPage.xaml.cs
@@ -0,0 +1,103 @@
+﻿namespace AnimalMatchingGame
+{
+    public partial class MainPage : ContentPage
+    {
+        
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void PlayAgainButton_Clicked(object sender, EventArgs e)
+        {
+            AnimalButtons.IsVisible = true;
+            PlayAgainButton.IsVisible = false;
+
+            List<string> animalEmoji = [
+                "🐙", "🐙",
+                "🕷", "🕷",
+                "🐶", "🐶",
+                "😸", "😸",
+                "🐮", "🐮",
+                "🐷", "🐷",
+                "🐟", "🐟",
+                "🦐", "🦐"
+                ];
+
+            foreach (var button in AnimalButtons.Children.OfType<Button>()){
+
+                int index = Random.Shared.Next(animalEmoji.Count);
+                string nextEmoji = animalEmoji[index];
+                button.Text = nextEmoji;
+                animalEmoji.RemoveAt(index);
+
+            }
+
+            Dispatcher.StartTimer(TimeSpan.FromSeconds(0.1), TimerTick);
+        }
+
+        int tenthOfSecondsElapsed = 0;
+        int bestTime = 0;
+        private bool TimerTick()
+        {
+            if (!this.IsLoaded) return false;
+
+            tenthOfSecondsElapsed += 1;
+
+            TimeElapsed.Text = "Time elapsed: " + (tenthOfSecondsElapsed / 10F).ToString("0.0s");
+
+            if (PlayAgainButton.IsVisible)
+            {
+                if (tenthOfSecondsElapsed < bestTime)
+                {
+                    bestTime = tenthOfSecondsElapsed;
+                    BestTime.Text = "Best recorded time: " + (bestTime / 10F).ToString("0.0s");
+                }
+
+                tenthOfSecondsElapsed = 0;
+                return false;
+            }
+
+            return true;
+
+        }
+
+        Button lastClicked;
+        bool findingMatch = false;
+        int matchesFound;
+
+        private void Button_Clicked(object sender, EventArgs e)
+            {
+            
+            if (sender is Button buttonClicked) {
+                if (!string.IsNullOrWhiteSpace(buttonClicked.Text) && (findingMatch == false)) {
+                    buttonClicked.BackgroundColor = Colors.Red;
+                    lastClicked = buttonClicked;
+                    findingMatch = true;
+                }
+                else
+                {
+                    if ((buttonClicked != lastClicked) && (buttonClicked.Text == lastClicked.Text) && (!String.IsNullOrWhiteSpace(buttonClicked.Text)))
+                    {
+                        matchesFound++;
+                        lastClicked.Text = " ";
+                        buttonClicked.Text = " ";
+                    }
+                    lastClicked.BackgroundColor = Colors.LightBlue;
+                    buttonClicked.BackgroundColor = Colors.LightBlue;
+                    findingMatch = false;
+                }
+                
+
+                if (matchesFound == 8)
+                {
+                    matchesFound = 0;
+                    AnimalButtons.IsVisible = false;
+                    PlayAgainButton.IsVisible = true;
+                }
+            }
+        }
+    }
+
+}
---
+++ MyFirstConsoleApp/MyFirstConsoleApp/Program.cs
@@ -0,0 +1,54 @@
+﻿
+OperatorExamples();
+TrySomeLoops();
+
+void OperatorExamples()
+{
+    int width = 3;
+
+    width++;
+
+    int height = 2 + 4;
+    int area = width * height;
+    Console.WriteLine(area);
+
+
+    while (area < 20)
+    {
+        height++;
+        area = width * height;
+    }
+
+    do
+    {
+        width--;
+        area = width * height;
+    } while (area > 25);
+
+    string result = "The area";
+    result = result + " is " + area;
+    Console.WriteLine(result);
+
+    bool truthValue = true;
+    Console.WriteLine(truthValue);
+
+    int p = 2;
+    for (int q = 2; q < 32; q = q * 2) {
+        while (p < q) {
+    p = p * 2;
+    }
+    q = p - q;
+    }
+
+}
+
+static void TrySomeLoops() {
+    int count = 0;
+    while (count < 10) {
+        count = count + 1;
+    }
+    for(int i=0; i<5; i++) {
+        count--;
+    }
+    Console.WriteLine("The answer is " + count);
+}
